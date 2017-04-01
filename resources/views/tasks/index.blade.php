fc2313a16f6742983fe027e9500218b30fe1abb0
Henrique Freitas <henriqusouza90@gmail.com>
2017-04-01 18:42:02 -0300

    -Adicionado view para retornar todas as tasks e tbm uma em específico

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+
+<ul>
+    @foreach($tasks as $task)
+        <li><a href="/tasks/{{ $task->id }}">{{ $task->body }}</a></li>
+    @endforeach
+</ul>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tasks', function () {
+
+    $tasks = DB::table('tasks')->get(); //mais opções em https://laravel.com/docs/5.4/queries
+
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+    $task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
 });
