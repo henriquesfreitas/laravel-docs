a84a6261b80205fdf232a7954416db872a8ec7cf
Henrique Freitas <henriqusouza90@gmail.com>
2017-04-08 15:48:58 -0300

    -Episódio 10 -Adicionado componentes do blade;

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ Route::get('/tasks', 'TasksController@index');
 
 Route::get('/tasks/{task}', 'TasksController@show');
 
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/{post}', 'PostController@show');
