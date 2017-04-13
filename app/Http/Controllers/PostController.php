52d7cd9db71460089e04dd7e49c05a06df605134
Henrique Freitas <henriqusouza90@gmail.com>
2017-04-12 22:39:51 -0300

    -Episódio 11; -Criado formulário;

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -12,5 +13,17 @@ class PostController extends Controller
 
     public function show(){
         return view('posts.show');
+    }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(){
+
+        Post::create(request(['title', 'body']));
+
+        return redirect('/');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,8 @@ Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/', 'PostController@index');
 
-Route::get('/posts/{post}', 'PostController@show');
+//Route::get('/posts/{post}', 'PostController@show');
+
+Route::get('/posts/create', 'PostController@create');
+
+Route::post('/posts', 'PostController@store');
