7a2d435177aa0c2b9bc337fa6f0f9f2754bc6908
Henrique Freitas <henriqusouza90@gmail.com>
2017-05-17 22:37:20 -0300

    -Episódio 22; -Realizado testes unitários;

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Unit;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -16,5 +19,27 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+        $this->get('/')->assertSee("The Bootstrap Blog");
+
+                $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create(['created_at' => \Carbon\Carbon::now()->subMonth()]);
+
+        $posts = Post::archives();
+
+        $this->assertCount(2, $posts);
+
+        $this->assertEquals([
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1,
+            ],
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1,
+            ],
+        ], $posts);
     }
+
 }
