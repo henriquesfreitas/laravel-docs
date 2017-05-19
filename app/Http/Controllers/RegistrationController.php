cdd60cb7afd83f8b517564fa431aaec43a6e4450
Henrique Freitas <henriqusouza90@gmail.com>
2017-05-18 21:13:25 -0300

    -Episódio 26; -Criado funcionalidade de envio de email;

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\WelcomeMail;
 use App\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class RegistrationController extends Controller
 {
@@ -23,7 +24,10 @@ class RegistrationController extends Controller
             'email' => request('email'),
             'password' => bcrypt(request('password'))
         ]);
+
         auth()->login($user);
+
+        Mail::to($user)->send(new WelcomeMail($user));
 
         return redirect()->home();
     }
